7dc99d6300bdfe000b1b7923122ebe92fd442d71
ValeriaSalas3004 <valesalas3004@gmail.com>
2026-06-01 22:56:32 -0600

    Crear método save en UserService

--- backend/recycling-campaign-system/src/main/java/com/example/recycling_campaign_system/Model/User.java
+++ backend/recycling-campaign-system/src/main/java/com/example/recycling_campaign_system/Model/User.java
@@ -2,8 +2,10 @@ package com.example.recycling_campaign_system.Model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "tb_users")
 public class User {
     @Id
     private Integer id;
--- backend/recycling-campaign-system/src/main/java/com/example/recycling_campaign_system/Service/UserService.java
+++ backend/recycling-campaign-system/src/main/java/com/example/recycling_campaign_system/Service/UserService.java
@@ -1,10 +1,25 @@
 package com.example.recycling_campaign_system.Service;
 
+import com.example.recycling_campaign_system.Model.User;
+import com.example.recycling_campaign_system.Repository.UserRepository;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-@Entity
+import java.util.Optional;
+
+@Service
 public class UserService {
+    @Autowired
+    private UserRepository repository;
 
-
+    public User save(User user){
+        Optional<User> opt = this.repository.findById(user.getId());
+        if (opt.isPresent()){
+            return null;
+        }
+        return this.repository.save(user);
+    }
+    
 }
