1c1fd24307ab73d32cee8a5710e2760bb8d32d03
ValeriaSalas3004 <valesalas3004@gmail.com>
2026-06-01 22:38:04 -0600

    Extends JpaRepository en UserRepository

--- backend/recycling-campaign-system/src/main/java/com/example/recycling_campaign_system/Repository/UserRepository.java
+++ backend/recycling-campaign-system/src/main/java/com/example/recycling_campaign_system/Repository/UserRepository.java
@@ -1,4 +1,14 @@
 package com.example.recycling_campaign_system.Repository;
 
-public interface UserRepository {
+import com.example.recycling_campaign_system.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
 }
