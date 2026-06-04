82089abddec0441c6079ca01a23c61ee2689c5d4
EnierAragon <91.enier.91@gmail.com>
2026-06-04 02:29:46 -0600

    JpaRepository basico

--- backend/recycling-campaign-system/src/main/java/com/example/recycling_campaign_system/Repository/WasteRepository.java
+++ backend/recycling-campaign-system/src/main/java/com/example/recycling_campaign_system/Repository/WasteRepository.java
@@ -1,4 +1,16 @@
 package com.example.recycling_campaign_system.Repository;
 
-public interface WasteRepository {
+import com.example.recycling_campaign_system.Model.WasteType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface WasteRepository extends JpaRepository<WasteType, Long> {
+
+    Optional<WasteType> findByWeight(Double weight);
+
+    Optional<WasteType> findByType(String type);
+
 }
--- backend/recycling-campaign-system/src/main/java/com/example/recycling_campaign_system/Repository/ZoneRepository.java
+++ backend/recycling-campaign-system/src/main/java/com/example/recycling_campaign_system/Repository/ZoneRepository.java
@@ -1,4 +1,16 @@
 package com.example.recycling_campaign_system.Repository;
 
-public interface ZoneRepository {
+import com.example.recycling_campaign_system.Model.RecollectionZone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ZoneRepository extends JpaRepository<RecollectionZone,Long> {
+
+    Optional<RecollectionZone> findByLocation(String location);
+
+    Optional<RecollectionZone> findBySchedule(String schedule);
+
 }
