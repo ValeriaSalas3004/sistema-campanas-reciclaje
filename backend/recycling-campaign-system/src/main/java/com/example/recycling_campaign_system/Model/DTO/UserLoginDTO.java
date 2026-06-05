6e5035df25b1910b16361323a89b6ed82d9b26a1
ValeriaSalas3004 <valesalas3004@gmail.com>
2026-06-04 22:16:22 -0600

    Módulo listo

--- backend/recycling-campaign-system/src/main/java/com/example/recycling_campaign_system/Model/DTO/UserLoginDTO.java
+++ backend/recycling-campaign-system/src/main/java/com/example/recycling_campaign_system/Model/DTO/UserLoginDTO.java
@@ -16,15 +16,9 @@ public class UserLoginDTO {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
 
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
