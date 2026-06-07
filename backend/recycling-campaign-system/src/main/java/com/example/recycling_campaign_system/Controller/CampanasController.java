ef309d4ccd9ba1083eb717a23ed2a74a9b16c3fa
redraccooon <eduardo12alberto@gmail.com>
2026-06-06 18:16:02 -0600

    Creación de los endpoints de Listar todas, Listar por id y agregar campañas.

--- backend/recycling-campaign-system/src/main/java/com/example/recycling_campaign_system/Controller/CampanasController.java
+++ backend/recycling-campaign-system/src/main/java/com/example/recycling_campaign_system/Controller/CampanasController.java
@@ -1,9 +1,46 @@
 package com.example.recycling_campaign_system.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.recycling_campaign_system.Model.Campanas;
+import com.example.recycling_campaign_system.Service.CampanasService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/campaigns")
 public class CampanasController {
+    @Autowired
+    private CampanasService service;
+
+    @GetMapping
+    public ResponseEntity<?> listCampanas(){
+        return ResponseEntity.ok(this.service.findAll());
+    }
+    @GetMapping("/{id}")
+    public Campanas findById(@PathVariable Integer id){
+        return this.service.findById(id);
+    }
+    @PostMapping
+    public ResponseEntity<?> addCampana(@Validated @RequestBody Campanas campana, BindingResult result) {
+        if (result.hasErrors()) {
+            Map<String, String> errors = new HashMap<>();
+            for (FieldError error : result.getFieldErrors()) {
+                errors.put(error.getField(), error.getDefaultMessage());
+            }
+            return ResponseEntity.badRequest().body(errors);
+        }
+        if(campana==null){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("La campaña con id "+campana.getId()+ " ya esta registrada.");
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(campana);
+    }
+
 }
--- backend/recycling-campaign-system/src/main/java/com/example/recycling_campaign_system/Repository/CampanasRepository.java
+++ backend/recycling-campaign-system/src/main/java/com/example/recycling_campaign_system/Repository/CampanasRepository.java
@@ -2,6 +2,10 @@ package com.example.recycling_campaign_system.Repository;
 
 import com.example.recycling_campaign_system.Model.Campanas;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CampanasRepository extends JpaRepository<Campanas, Integer> {
+
+
 }
