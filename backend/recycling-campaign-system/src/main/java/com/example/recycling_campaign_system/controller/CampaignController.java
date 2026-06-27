5e14f3b6a012d46a927b15578250d1e85d151c99
ValeriaSalas3004 <valesalas3004@gmail.com>
2026-06-27 00:00:07 -0600

    correción url en backend

--- backend/recycling-campaign-system/src/main/java/com/example/recycling_campaign_system/controller/CampaignController.java
+++ backend/recycling-campaign-system/src/main/java/com/example/recycling_campaign_system/controller/CampaignController.java
@@ -16,7 +16,7 @@ import java.util.Map;
 
 @CrossOrigin(origins = "*")
 @RestController
-@RequestMapping("/campaigns")
+@RequestMapping("api/campaigns")
 public class CampaignController {
     @Autowired
     private CampaignService service;
